7cfd944f2bfa94c788bda37e8484b5f5fb08241d
Nadav Shwarz <nadave.19933@gmail.com>
2022-02-04 17:05:38 +0200

    New OOP design for the C++ project
    New OOP design for the C++ project.

--- C++/Project1/Header/Matrix_Operations.h
+++ C++/Project1/Header/Matrix_Operations.h
@@ -1,152 +1,115 @@
 #ifndef MATRIX_OPERATIONS
 #define MATRIX_OPERATIONS
 
-//Constants
-#define PI 3.14159265358979323846   // pi
-#define TOTAL 1.0e-9
-
-#include <functional>
-
-using namespace std;
-using Vector = std::vector<double>;
-using Matrix = std::vector<std::vector<double>>;
-
-namespace Matrix_Operations {
-
-	//Method for print vectors/matrix and for reading input file:
-
-/*
-	print_vec method print the given vector to the console.
-*/
-	void print_vec(const Vector& v);
+#include "Header.h"
+#include "Operators.h"
+using namespace Operators;
 
 
-	/*
-		print_matrix method print the given matrix to the console.
-	*/
-	void print_matrix(const Matrix& matrix);
+namespace Matrix_Operations_ {
+
+	//This class contain methods for Matrix Operation:
+	class Matrix__Operations 
+	{
+	public:
+
+		/*
+		generate_matrix method randomly generate matrix.
+		*/
+		static Matrix generate_matrix(int size);
+
+		/*
+			This method return the vetor length.
+		*/
+		double vec_length(const Vector& v);
 
 
-	/*
-		read_file method read the all content from the input file.
-		The method return matrix with all the values from the input file.
-	*/
-	Matrix read_file(const std::string& file_name);
+		/*
+		This method performe the matrix transpose operation.
+		Before transpose operation:
+		{ {1.2,3.4,4},
+		  {3,4,5.5},
+		  {2.2, 5, 6}
+		 }
+
+		 After transpose operation:
+		 { {1.2, 3, 2.2},
+		   {3.4, 4, 5},
+		   {4, 5.5, 6}
+		 }
+		*/
+		void transpose(Matrix& matrix);
+
+		/*
+			dot_product method perform matrix multiplication, and return the result.
+		*/
+		Matrix dot_product
+		(const Matrix& matrix1, Matrix matrix2, bool check = false);
 
 
-	/*
-		read_line method get string value and convert it to vector of double.
-	*/
-	Vector read_line(std::string line);
+		/*
+			dot_product method return the result of the multiplication between vector1 and vector2.
+		*/
+		double dot_product(const Vector& v1, const Vector& v2,
+			bool check = false);
 
 
-	/*
-	write_to_file method write the content of the given matrix to the file.
-	The method return True if succeeded.
-	*/
-	bool write_to_file(const std::string& file_name, const Matrix& mtx);
+		/*
+			dot_product method return the result of the multiplication between given scalar and vector.
+		*/
+		Vector dot_product(double scalar, Vector& v);
 
 
-	/*
-	generate_matrix method randomly generate matrix.
-	*/
-	Matrix generate_matrix(int size);
-
-	//Methods for Matrix Operation:
-
-	/*
-		This method return the vetor length.
-	*/
-	double vec_length(const Vector& v);
+		/*
+			rotation_product method execute matrix multiplication of regular matrix and rotation matrix.
+		*/
+		Matrix rotation_product(Matrix& matrix,
+			Matrix rotation, int p, int q);
 
 
-	/*
-	This method performe the matrix transpose operation.
-	Before transpose operation:
-	{ {1.2,3.4,4},
-	  {3,4,5.5},
-	  {2.2, 5, 6}
-	 }
-
-	 After transpose operation:
-	 { {1.2, 3, 2.2},
-	   {3.4, 4, 5},
-	   {4, 5.5, 6}
-	 }
-	*/
-	void transpose(Matrix& matrix);
+		/*
+			diagonal_multiplication method execute matrix multiplication of regular matrix
+			and diagonal matrix.
+		*/
+		Matrix diagonal_multiplication(Matrix& matrix,
+			const Matrix& diagonal);
 
 
-	/*
-		dot_product method perform matrix multiplication, and return the result.
-	*/
-	Matrix dot_product
-	(const Matrix& matrix1, Matrix matrix2,
-		std::function<void(double&)> F = nullptr);
+		/*
+			eye_matrix method return the identity matrix of size n.
+		*/
+		Matrix eye_matrix(int n);
 
 
-	/*
-		dot_product method return the result of the multiplication between vector1 and vector2.
-	*/
-	double dot_product(const Vector& v1, const Vector& v2, std::function<void(double&)> F = nullptr);
+		/*
+			get_diag method return vector that contains the elements that on the matrix diagonal.
+		*/
+		Vector get_diagonal_matrix(const Matrix& matrix);
 
 
-	/*
-		dot_product method return the result of the multiplication between given scalar and vector.
-	*/
-	Vector dot_product(double scalar, Vector& v);
+		/*
+			diag_to_matrix method convert a vector to diagonal matrix in size size_val value.
+		*/
+		Matrix diagonal_to_matrix(const Vector& diagonal, int size_val);
 
 
-	/*
-		rotation_product method execute matrix multiplication of regular matrix and rotation matrix.
-	*/
-	Matrix rotation_product(Matrix& matrix,
-		Matrix rotation, int p, int q);
+		/*
+			get_column method get matrix and column. The method rturn vector
+			with the elements of this matrix's column.
+		*/
+		Vector get_column(const Matrix& matrix, int column);
 
 
-	/*
-		diagonal_multiplication method execute matrix multiplication of regular matrix
-		and diagonal matrix.
-	*/
-	Matrix diagonal_multiplication(Matrix& matrix,
-		const Matrix& diagonal);
+		/*
+			This method return a copy of the array collapsed into one dimension.
+		*/
+		Vector flatten(const Matrix& matrix);
 
 
-	/*
-		eye_matrix method return the identity matrix of size n.
-	*/
-	Matrix eye_matrix(int n);
+	private:
 
+	};
 
-	/*
-		get_diag method return vector that contains the elements that on the matrix diagonal.
-	*/
-	Vector get_diag(const Matrix& matrix);
-
-
-	/*
-		diag_to_matrix method convert a vector to diagonal matrix in size size_val value.
-	*/
-	Matrix diag_to_matrix(const Vector& diagonal, int size_val);
-
-
-	/*
-		get_column method get matrix and column. The method rturn vector
-		with the elements of this matrix's column.
-	*/
-	Vector get_column(const Matrix& matrix, int column);
-
-
-	/*
-		This method return a copy of the array collapsed into one dimension.
-	*/
-	Vector flatten(const Matrix& matrix);
-
-
-	/*
-		my_round check if num is smaller than TOTAL value. If so, then num become zero.
-	*/
-	void my_round(double& num);
-
+	
 };
 #endif // !MATRIX_OPERATIONS
--- C++/Project1/Header/QR_Decomposition.h
+++ C++/Project1/Header/QR_Decomposition.h
@@ -1,44 +1,60 @@
 #ifndef QR_DECOMPOSITION
 #define QR_DECOMPOSITION
 
-using namespace std;
+#include "Header.h"
+#include "Operators.h"
+#include "Matrix_Operations.h"
+using namespace Operators;
+using namespace Matrix_Operations_;
 
-namespace QR_Decomposition {
 
-	/*
+namespace QR_Decomposition_ {
+
+	class QR_Decomposition : public Matrix__Operations
+	{
+	public:
+
+		/*
 		For giveen matrix A[a1,a2,...,an] in size n x n
 		Execute Gram Schmidt process, and return new matrix.
 	*/
-	Matrix Gram_Schmidt_process(Matrix matrix);
+		Matrix Gram_Schmidt_process(Matrix matrix);
 
 
-	/*
-		Q is an orthogonal matrix, that created by normalize
-		the columns of the matrix that has been received
-		from Gram Schmidt process.
-	*/
-	Matrix Q_matrix(Matrix U);
+		/*
+			Q is an orthogonal matrix, that created by normalize
+			the columns of the matrix that has been received
+			from Gram Schmidt process.
+		*/
+		Matrix Q_matrix(Matrix U);
 
 
-	/*
-		R is an upper triangular matrix.
-		R produce by multiplication of Q transpose and input_matrix.
-	*/
-	Matrix R_matrix(Matrix input_matrix, Matrix Q);
+		/*
+			R is an upper triangular matrix.
+			R produce by multiplication of Q transpose and input_matrix.
+		*/
+		Matrix R_matrix(Matrix input_matrix, Matrix Q);
 
 
-	/*
-		Calculation of QR decomposition of the given matrix.
-	*/
-	void QR_decomposition(const Matrix& input_matrix);
+		/*
+			Calculation of QR decomposition of the given matrix.
+		*/
+		void QR_decomposition(const Matrix& input_matrix);
 
 
-	/*
-		Checking that the Q * R is equal to input matrix.
-	*/
-	void check_decomposition(const Matrix& Q,
-		const Matrix& R);
+		/*
+			Checking that the Q * R is equal to input matrix.
+		*/
+		void check_decomposition(const Matrix& Q,
+			const Matrix& R);
 
+	private:
+		/*
+		Execute projection operation between two vectors.
+		*/
+		inline Vector projection(Vector u, Vector v1);
+	};
+	
 };
 
 #endif // !QR_DECOMPOSITION
